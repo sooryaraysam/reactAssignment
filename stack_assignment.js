4819a2a5332fa5ad7b0359591eb1c482bff13b75
sooryaraysam <sooryaraysam@gmail.com>
2022-02-27 13:13:21 +0100

    Push method implemented

--- stack_assignment.js
+++ stack_assignment.js
@@ -10,4 +10,20 @@ constructor(total_size)
     this.top = -1;
     this.total_size = total_size;             
 }
+
+push(ele)    
+{
+if(this.top==this.total_size-1)
+{
+    console.log("Cannot insert! Stack overflow")
+}
+else
+{
+    this.top = this.top + 1;
+    this.array[this.top] = ele;
+    console.log(`The element being pushed is ${ele}`)
+    return this.top;
+}
+}
+
 }
